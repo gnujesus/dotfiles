2f3d54cb50716e3343369874d49b6e06ca1b0d3d
gnujesus <jotamartinezd@gmail.com>
2026-06-22 15:00:35 -0400

    changed theme

--- nvim/lua/custom/plugins/hydra.lua
+++ nvim/lua/custom/plugins/hydra.lua
@@ -1,61 +1,63 @@
-return {
-  'nvimtools/hydra.nvim',
-  config = function()
-    local Hydra = require 'hydra'
+return {}
 
-    local hint = [[
- ^^^^^^^^     Resize
- ^^^^^^^^^^──────────────
- ^ ^ _k_ ^ ^   Increase height
- _h_ ^ ^ _l_   Resize width
- ^ ^ _j_ ^ ^   Decrease height
-     _<Esc>_ to exit
-]]
-
-    Hydra {
-      name = 'Resize Splits',
-      hint = hint,
-      config = {
-        color = 'pink',
-        invoke_on_body = true,
-        hint = {},
-      },
-      mode = 'n',
-      body = '<leader>r',
-      heads = {
-
-        {
-          'h',
-          function()
-            vim.cmd 'vertical resize -5'
-          end,
-          { desc = '←' },
-        },
-        {
-          'l',
-          function()
-            vim.cmd 'vertical resize +5'
-          end,
-          { desc = '→' },
-        },
-        {
-          'j',
-          function()
-            vim.cmd 'resize -2'
-          end,
-          { desc = '↓' },
-        },
-        {
-          'k',
-          function()
-            vim.cmd 'resize +2'
-          end,
-          { desc = '↑' },
-        },
-        { '<Esc>', nil, { exit = true, desc = 'Exit' } },
-
-        { '<C-c>', nil, { exit = true, nowait = true, desc = 'Exit' } },
-      },
-    }
-  end,
-}
+-- return {
+--   'nvimtools/hydra.nvim',
+--   config = function()
+--     local Hydra = require 'hydra'
+--
+--     local hint = [[
+--  ^^^^^^^^     Resize
+--  ^^^^^^^^^^──────────────
+--  ^ ^ _k_ ^ ^   Increase height
+--  _h_ ^ ^ _l_   Resize width
+--  ^ ^ _j_ ^ ^   Decrease height
+--      _<Esc>_ to exit
+-- ]]
+--
+--     Hydra {
+--       name = 'Resize Splits',
+--       hint = hint,
+--       config = {
+--         color = 'pink',
+--         invoke_on_body = true,
+--         hint = {},
+--       },
+--       mode = 'n',
+--       body = '<leader>r',
+--       heads = {
+--
+--         {
+--           'h',
+--           function()
+--             vim.cmd 'vertical resize -5'
+--           end,
+--           { desc = '←' },
+--         },
+--         {
+--           'l',
+--           function()
+--             vim.cmd 'vertical resize +5'
+--           end,
+--           { desc = '→' },
+--         },
+--         {
+--           'j',
+--           function()
+--             vim.cmd 'resize -2'
+--           end,
+--           { desc = '↓' },
+--         },
+--         {
+--           'k',
+--           function()
+--             vim.cmd 'resize +2'
+--           end,
+--           { desc = '↑' },
+--         },
+--         { '<Esc>', nil, { exit = true, desc = 'Exit' } },
+--
+--         { '<C-c>', nil, { exit = true, nowait = true, desc = 'Exit' } },
+--       },
+--     }
+--   end,
+-- }
--- nvim/lua/custom/plugins/noirbuddy.lua
+++ nvim/lua/custom/plugins/noirbuddy.lua
@@ -1,25 +1,27 @@
-return {
-  'jesseleite/nvim-noirbuddy',
-  dependencies = {
-    { 'tjdevries/colorbuddy.nvim' },
-  },
-  lazy = false,
-  priority = 1000,
-  install = {
-    colorscheme = { 'noirbuddy' },
-  },
-  opts = {
-    preset = 'miami-nights',
-    -- colors = {
-    --   background = '#131515',
-    --   primary = '#adf7b6',
-    --   secondary = '#FF56AB',
-    -- },
-    stles = {
-      italic = true,
-      bold = false,
-      underline = false,
-      undercurl = true,
-    },
-  },
-}
+return {}
+
+-- return {
+--   'jesseleite/nvim-noirbuddy',
+--   dependencies = {
+--     { 'tjdevries/colorbuddy.nvim' },
+--   },
+--   lazy = false,
+--   priority = 1000,
+--   install = {
+--     colorscheme = { 'noirbuddy' },
+--   },
+--   opts = {
+--     preset = 'miami-nights',
+--     -- colors = {
+--     --   background = '#131515',
+--     --   primary = '#adf7b6',
+--     --   secondary = '#FF56AB',
+--     -- },
+--     stles = {
+--       italic = true,
+--       bold = false,
+--       underline = false,
+--       undercurl = true,
+--     },
+--   },
+-- }
